6b0102df5b3ccedf317adbfacedfc2cb4f56fc4e
takebul <takebulislam@gmail.com>
2026-04-02 08:51:09 +0600

    Added font-family, lucide-icons, navbar component & changed theme color.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">
-        Welcome To Digi-Tools Platform
-      </h1>
+      <Navbar />
     </>
   );
 }
